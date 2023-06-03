0be4d119e8641446a28750df7e5b11554f6c989f
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-06-03 11:30:37 -0400

    notes

--- notes.js
+++ notes.js
@@ -4,7 +4,16 @@
 // push() adds an element to the end of an array
 // shift() removes the first element
 // unshift() adds an element to the beginning of the array
-
+// new set() The Set object lets you store unique values of any type, whether primitive values or object references. 
+    // const mySet1 = new Set();, 
+    // mySet1.add(1); // Set(1) { 1 }, 
+    // mySet1.has(1); // true, 
+    // mySet1.delete(1); // removes 1 from the set
+    // mySet1.size; // 1
+   // https://structy.net/problems/has-path
+//    https://masteringjs.io/tutorials/fundamentals/foreach-object   explains how to forEach() on objects.
+//     Basically Object.keys(target) or Object.values(target) or Object.entry(target)
+// ------------------------------------------------------
 
 const graph = {
     a:['b','c'],
@@ -15,12 +24,12 @@ const graph = {
     f:[]
 };
 
-const depthFirstPrint = (graph, source) => {    //iterative
+const depthFirstPrintIterative = (graph, source) => {    //iterative
     const stack = [source];
 
     while (stack.length > 0){
         const current = stack.pop();
-        console.log(current);
+        console.log('depthFirstPrintIterative',current);
 
         for (let neighbour of graph[current]){
             stack.push(neighbour);
@@ -28,24 +37,40 @@ const depthFirstPrint = (graph, source) => {    //iterative
     }
 };
 
-// const depthFirstPrint = (graph, source) => { //recursive
-//     console.log(source);
-//     for (let neighbour of graph [source]){
-//         depthFirstPrint(graph, neighbour);
-//         }
-//     };
+const depthFirstPrintRecursive = (graph, source) => { //recursive
+    console.log('depthFirstPrintRecursive',source);
+    for (let neighbour of graph [source]){
+        depthFirstPrintRecursive(graph, neighbour);
+        }
+    };
 
-// const breathFirstPrint = (graph, source) => { //only itirative for BFS
-//     const queue = [source];
-//     while (queue.length > 0) {
-//         const current = queue.shift();
-//         console.log(current);
-//         for (let neighbour of graph[current]) {
-//             queue.push(neighbour)
-//         }
-//     }
-// };
+const breathFirstPrint = (graph, source) => { //only itirative for BFS
+    const queue = [source];
+    while (queue.length > 0) {
+        const current = queue.shift();
+        console.log('breathFirstPrintIterative',current);
+        for (let neighbour of graph[current]) {
+            queue.push(neighbour)
+        }
+    }
+};
 
-// breathFirstPrint(graph,'a');
 
-depthFirstPrint(graph, 'a');
+let content = document.getElementsByClassName('content')[0];
+Object.entries(graph).forEach(entry => {
+    const [key, value] = entry;
+    let displayEntry = document.createElement('div');
+    displayEntry.textContent = `${key}:  ${value} `
+    content.appendChild(displayEntry); 
+  });
+
+
+
+
+
+
+depthFirstPrintIterative(graph, 'a');
+console.log('//');
+depthFirstPrintRecursive(graph, 'a');
+console.log('//');
+breathFirstPrint(graph,'a');
